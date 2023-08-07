b9266463bda461f7bdb21d2a55d234ab67db7541
Jozef Pupava <jpupava@suse.com>
2023-08-07 20:31:25 +0200

    12-SP3 tomcat on SAP-installtest

--- tests/qam-updinstall/update_install.pm
+++ tests/qam-updinstall/update_install.pm
@@ -149,7 +149,8 @@ sub run {
     my @modules = split(/,/, $repos);
     foreach (@modules) {
         # substitue SLES_SAP for LTSS repo at this point is SAP ESPOS
-        $_ =~ s/SAP_(\d+(-SP\d)?)/$1-LTSS/ if is_sle('15+');
+        $_ =~ s/ppc64le/x86_64/;
+        $_ =~ s/SAP_(\d+(-SP\d)?)/SERVER_$1-LTSS-TERADATA/;
         next if s{http.*SUSE_Updates_(.*)/?}{$1};
         die 'Modules regex failed. Modules could not be extracted from repos variable.';
     }
