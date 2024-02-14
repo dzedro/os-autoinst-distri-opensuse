57a6598240f483a99e2fd075073959d30b738b86
Jozef Pupava <jpupava@suse.com>
2024-02-14 10:10:03 +0100

    mdadm: Use fixed kernel bsc#1219073

--- tests/console/mdadm.pm
+++ tests/console/mdadm.pm
@@ -16,10 +16,23 @@ use serial_terminal 'select_serial_terminal';
 use version_utils 'is_sle';
 use strict;
 use warnings;
+use power_action_utils qw(power_action);
+use utils;
 
 sub run {
+    my ($self) = @_;
     select_serial_terminal;
     my $timeout = 360;
+
+    if (is_sle('=15-sp4') && check_var('ARCH', 'aarch64')) {
+        assert_script_run("sed -i 's/set timeout=-1/set timeout=2/' /boot/grub2/grub.cfg");
+        zypper_call('ar -G -f http://download.suse.de/ibs/home:/colyli:/branches:/SUSE:/SLE-15-SP4:/Update/SUSE_SLE-15-SP4_Update/ mdadm_fix');
+        zypper_call('rm kernel-default*');
+        zypper_call('in -f --from mdadm_fix kernel-default');
+        power_action 'reboot', textmode => 1;
+        $self->wait_boot;
+        select_serial_terminal;
+    }
 
     assert_script_run 'wget ' . data_url('qam/mdadm.sh');
     if (is_sle('<15')) {
@@ -32,6 +45,14 @@ sub run {
         assert_script_run 'bash mdadm.sh |& tee mdadm.log; if [ ${PIPESTATUS[0]} -ne 0 ]; then false; fi', $timeout;
     }
     upload_logs 'mdadm.log';
+
+    if (is_sle('=15-sp4') && check_var('ARCH', 'aarch64')) {
+        zypper_call('rr mdadm_fix');
+        zypper_call('up --allow-vendor-change kernel-default');
+        power_action 'reboot', textmode => 1;
+        $self->wait_boot;
+        select_serial_terminal;
+    }
 }
 
 sub post_fail_hook {
