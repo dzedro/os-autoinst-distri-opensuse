31293beb49119805ee663cf0fe8ba562d2ea427b
Jozef Pupava <jpupava@suse.com>
2024-03-25 15:40:31 +0100

    1

--- tests/console/sudo.pm
+++ tests/console/sudo.pm
@@ -105,8 +105,12 @@ sub run {
     zypper_call 'in sudo expect';
     select_console 'user-console';
     # Defaults targetpw -> asks for root PW
-    my $exp_user = (is_azure && is_sle('>=15-SP4')) ? 'bernhard is not in the sudoers file' : 'root';
-    validate_script_output("expect -c 'spawn sudo id -un;expect password {send \"$testapi::password\\r\";interact}'", sub { qr/^$exp_user\$/ });
+#    my $exp_user = (is_azure && is_sle('>=15-SP4')) ? 'bernhard is not in the sudoers file' : 'root';
+#    validate_script_output("expect -c 'spawn sudo id -un;expect password {send \"$testapi::password\\r\";interact}'", sub { qr/^$exp_user\$/ });
+    # Check if sudo asks for the root password.
+    # On PublicCloud 'Defaults targetpw' is disabled. There sudo is expected to ask for the user password
+    my $exp_user = is_public_cloud ? "$testapi::username" : "root";
+    validate_script_output("expect -c 'spawn sudo id -un;expect password for $exp_user {send \"$testapi::password\\r\";interact}'", sub { qr/^root\$/ });
     foreach my $num (0, 1) {
         record_info "iteration $num";
         select_console 'root-console';
