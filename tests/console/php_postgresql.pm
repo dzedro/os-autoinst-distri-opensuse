fdda5a2ff99c0d1e8ac3d2d81bb0785187335ea0
Jozef Pupava <jpupava@suse.com>
2026-02-02 08:34:53 +0100

    php_postgresql: Workaround

--- tests/console/php_postgresql.pm
+++ tests/console/php_postgresql.pm
@@ -47,7 +47,7 @@ sub run {
     setup_apache2(mode => uc($php));
 
     # install requirements, all postgresql versions to test db upgrade if there are multiple versions
-    zypper_call 'in ' . $php . '-pgsql postgresql*-contrib sudo unzip';
+    zypper_call 'in ' . $php . '-pgsql postgresql17-contrib sudo unzip';
 
     # start postgresql service
     systemctl 'start postgresql';
